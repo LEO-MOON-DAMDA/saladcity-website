728fec8933e45b2c1c1d12b8845f50cb61f6f761
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-14 17:44:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<SaladcityHome />} />
-        <Route path="/menu" element={<MenuPage />} />
       </Routes>
     </Router>
   );
