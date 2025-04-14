442fc437086a5507359a9df798a6dc1a4375423a
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-14 13:19:05 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
-import PrintableMenu from "./PrintableMenu";
 import CustomPrintableMenu from "./CustomPrintableMenu";
 
 
