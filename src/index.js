5464f3d0c06d4a09f91e78d9185d39b5d36c0d8a
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-14 13:35:42 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import SaladcityHome from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <SaladcityHome />
+  </React.StrictMode>
+);
 
