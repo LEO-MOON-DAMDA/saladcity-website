410424378058b37c50678d0832fbed89b2989e8b
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-16 03:03:23 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import SaladcityHome from "./SaladcityHome";
 import MenuPage from "./components/MenuPage";
+// import NotFound from "./components/NotFound"; // (선택) 추후 생성 시 사용
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* 공통 레이아웃을 사용하는 페이지들 */}
+        {/* 공통 레이아웃이 적용되는 페이지 그룹 */}
         <Route element={<Layout />}>
-          <Route path="/" element={<SaladcityHome />} />     {/* 홈 */}
-          <Route path="/menu" element={<MenuPage />} />      {/* 메뉴 페이지 */}
+          <Route path="/" element={<SaladcityHome />} />
+          <Route path="/menu" element={<MenuPage />} />
+          {/* 향후 /mission, /market, /catering 등도 여기에 추가 */}
         </Route>
 
-        {/* 추후 404 페이지 만들 경우 대비 (선택) */}
+        {/* 선택: 없는 경로 처리 */}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
