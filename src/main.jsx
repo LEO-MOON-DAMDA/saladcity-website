5624e148566f8d02a943a2656c67d973146a7367
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-16 07:06:55 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SaladcityHome from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // ✅ App.jsx는 Router & Routes 포함된 라우터 최상단 컴포넌트
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SaladcityHome />
+    <App />
   </React.StrictMode>
-)
+);
