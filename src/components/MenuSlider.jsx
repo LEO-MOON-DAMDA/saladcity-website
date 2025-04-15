b7452e024838fd5d14664e02121a2aadc53514a6
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-15 18:24:42 +0900

    Update MenuSlider.jsx

--- src/components/MenuSlider.jsx
+++ src/components/MenuSlider.jsx
@@ -1,3 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./MenuSlider.css";
+
 export default function MenuSlider({ items }) {
   const [current, setCurrent] = useState(0);
   const timeoutRef = useRef(null);
