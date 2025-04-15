067cc8bda37396e67976c05c5fa4b0cc918e04b5
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-16 04:32:36 +0900

    Update FeaturedMenuSection.jsx

--- src/components/FeaturedMenuSection.jsx
+++ src/components/FeaturedMenuSection.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import FeaturedMenuCard from "./FeaturedMenuCard";
+import "./FeaturedMenuSection.css";
+
+export default function FeaturedMenuSection({ items }) {
+  return (
+    <section className="featured-menu-section">
+      <h2 className="featured-title">샐러드시티 추천 메뉴</h2>
+
+      <div className="featured-slider-wrapper">
+        <div className="featured-slider-scroll">
+          {items.map((item, idx) => (
+            <FeaturedMenuCard key={idx} item={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
 
