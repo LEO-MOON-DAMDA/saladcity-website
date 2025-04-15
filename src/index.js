8e1d7c0dae3aaed9458ec3e6146ed4ffc24d1cca
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-16 02:55:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import SaladcityHome from "./App";
+import App from "./App"; // ✅ 전체 앱 라우팅을 포함한 App 컴포넌트
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SaladcityHome />
+    <App />
   </React.StrictMode>
 );
-
