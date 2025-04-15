048a3e28c3642838854047eaaa17c214ff6dbae8
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-16 06:28:55 +0900

    Update InstaFeedSection.jsx

--- src/components/InstaFeedSection.jsx
+++ src/components/InstaFeedSection.jsx
@@ -1,1 +1,26 @@
+// src/components/InstaFeedSection.jsx
+import React from "react";
+import "./InstaFeedSection.css";
+
+const images = [
+  "/images/insta01.jpg",
+  "/images/insta02.jpg",
+  "/images/insta03.jpg",
+  "/images/insta04.jpg"
+];
+
+export default function InstaFeedSection() {
+  return (
+    <section className="insta-section">
+      <h2>#Saladcity on Instagram</h2>
+      <div className="insta-grid">
+        {images.map((src, index) => (
+          <div key={index} className="insta-image-wrapper">
+            <img src={src} alt={`insta${index + 1}`} className="insta-image" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
 
