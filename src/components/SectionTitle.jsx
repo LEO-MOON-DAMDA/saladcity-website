889d317029dc2bafe328a1cb9d442ac7709d203a
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-18 00:58:07 +0900

    Update SectionTitle.jsx

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,13 +3,13 @@ import "./SectionTitle.css";
 
 export default function SectionTitle({ children, style = {} }) {
   const combinedStyle = {
-    marginTop: "64px",
-    marginBottom: "4px",
+    marginTop: "96px",         // 상단 섹션과 충분히 띄움
+    marginBottom: "4px",       // 본 섹션과 거의 붙임
     paddingLeft: style.textAlign === "left" ? "12px" : "0px",
     paddingRight: style.textAlign === "right" ? "12px" : "0px",
     fontSize: "24px",
     fontWeight: "700",
-    color: "#2f855a", // 초록색 복원
+    color: "#2f855a",
     ...style,
   };
 
