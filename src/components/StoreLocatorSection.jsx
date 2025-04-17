bbab3967ee217b32f92b5f63d5300e5b1ae85bb4
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-17 21:43:30 +0900

    Update StoreLocatorSection.jsx

--- src/components/StoreLocatorSection.jsx
+++ src/components/StoreLocatorSection.jsx
@@ -35,7 +35,7 @@ const storeData = [
 
 export default function StoreLocatorSection({ showMap = true }) {
   return (
-    <section className="store-locator-section">
+    <section className="store-locator-section" style={{ paddingTop: 0 }}>
       <p className="sub-text">샐러드시티의 신선함을 직접 느낄 수 있는 오프라인 매장을 확인하세요.</p>
 
       <div className="store-info">
@@ -60,7 +60,7 @@ export default function StoreLocatorSection({ showMap = true }) {
 
         {showMap && (
           <div className="store-map">
-            <img src="/images/store-map-full.jpg" alt="샐러드시티 전체 매장 지도" />
+            <img src="/public/images/store-map-full.jpg" alt="샐러드시티 전체 매장 지도" />
           </div>
         )}
       </div>
