cc067269b2507f382788582c500176c1fed50abb
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-18 02:56:56 +0900

    Update SaladcityHome.jsx

--- src/SaladcityHome.jsx
+++ src/SaladcityHome.jsx
@@ -83,7 +83,7 @@ export default function SaladcityHome() {
 
         <ScrollingBanner />
 
-        <FeaturedMenuSection />
+        <FeaturedMenuSection items={homepageMenuItems} />
         <OurMissionSection />
         <SubscriptionSection />
         <ReviewSection />
