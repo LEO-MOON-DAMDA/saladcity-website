bfdfbeaf1ad300b7f2720b9d2bd2e05a2d2a939e
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-18 03:59:31 +0900

    Update SubscriptionSection.jsx

--- src/components/SubscriptionSection.jsx
+++ src/components/SubscriptionSection.jsx
@@ -10,35 +10,22 @@ export default function SubscriptionSection() {
         <img
           src="/images/1salcymission04.jpg"
           alt="정기식 이미지"
-          style={{ width: "100%", borderRadius: "16px" }}
         />
       </div>
 
-      <div className="subscription-text" style={{ textAlign: "right" }}>
+      <div className="subscription-text">
         <SectionTitle style={{ textAlign: "right" }}>
           정기식 구독으로 더 간편하게
         </SectionTitle>
 
-        <p
-          style={{
-            fontSize: "18px",
-            lineHeight: "1.8",
-            color: "#222",
-            marginBottom: "0",
-          }}
-        >
+        <p>
           매일 신선하게 준비된 샐러드를<br />
           원하는 주기에 맞춰 배송받으세요.
-          <br />
-          <br />
+          <br /><br />
           원하는 요일, 원하는 시간, 원하는 스타일<br />
           당신만을 위한 샐러드가 문 앞에 도착합니다.
-          <br />
-          <br />
-          <span
-            className="subscription-sub"
-            style={{ fontSize: "15px", color: "#666", fontStyle: "italic" }}
-          >
+          <br /><br />
+          <span className="subscription-sub">
             Delivering wellness, one bowl at a time.
           </span>
         </p>
