6373c5131727140907f233752ce619a86efd3477
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-17 17:07:03 +0900

    Update ScrollingBanner.jsx

--- src/components/ScrollingBanner.jsx
+++ src/components/ScrollingBanner.jsx
@@ -3,15 +3,18 @@ import "./ScrollingBanner.css";
 
 export default function ScrollingBanner() {
   const slogans = [
-    "Howdy, y’all Welcome to the safest City on Earth.",
-    "I wasn’t born in SaladCity, BUT I’ll DIE HERE. BORN & RAISED. ORGANICALLY GROWN.",
+    "Howdy, y’all Welcome to the safest City on Earth",
+    "I wasn’t born in SaladCity, BUT I’ll DIE HERE",
+    "BORN & RAISED. ORGANICALLY GROWN",
     "Where SALADS become legends.",
-    "Everything’s fresher IN SALADCITY – EVEN OUR CREW!. WE live in SALADCITY! ",
-    "Don’t mess with fresh. Big Bowls, Bigger Hearts, SALADCITY STYLE.",
-    "As Bold as Our Greens."
+    "Everything’s fresher IN SALADCITY – EVEN OUR CREW!",
+    "WE live in SALADCITY!",
+    "Don’t mess with fresh",
+    "Big Bowls Bigger Hearts, SALADCITY STYLE",
+    "As Bold as Our Greens"
   ];
 
-  const fullText = slogans.join(" · ") + " · "; // 마지막에도 점 찍고 반복 자연스럽게
+  const fullText = slogans.join(" · ") + "  ·  "; // 마지막에도 점 찍고 반복 자연스럽게
 
   return (
     <div className="scrolling-banner-wrapper">
