796425480b6d90edfda1906a2c4fe88a923121c9
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-18 01:12:51 +0900

    Update ScrollingBanner.jsx

--- src/components/ScrollingBanner.jsx
+++ src/components/ScrollingBanner.jsx
@@ -14,8 +14,8 @@ export default function ScrollingBanner() {
     "As Bold as Our GREENS"
   ];
 
-  const fullText = slogans.join("  ·  "); // 점 3칸 간격만 유지
-  const repeated = Array(20).fill(fullText).join("     "); // 반복 시에도 간격 넣기
+  const fullText = slogans.join("    ·    "); // 점 3칸 간격만 유지
+  const repeated = Array(20).fill(fullText).join("       "); // 반복 시에도 간격 넣기
 
   return (
     <div className="scrolling-banner-wrapper">
