553b0dcbc082eda56a10ec0553674eeea6c16f08
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-18 01:38:46 +0900

    Update SubTitle.jsx

--- src/components/SubTitle.jsx
+++ src/components/SubTitle.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function SubTitle({ children, style = {} }) {
   const combinedStyle = {
-    fontSize: "16px",
+    fontSize: "15px",
     fontWeight: "500",
-    color: "#666",
-    marginTop: "8px",
+    color: "#999999",
+    marginTop: "0px",
     marginBottom: "4px",
     paddingLeft: style.textAlign === "left" ? "12px" : "0px",
     paddingRight: style.textAlign === "right" ? "12px" : "0px",
