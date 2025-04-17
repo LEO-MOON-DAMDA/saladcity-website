047e111173c35e9abae600222b3a96ce4aee714d
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-17 17:44:16 +0900

    Create BrandButton.jsx

---
+++ src/components/BrandButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./BrandButton.css";
+
+export default function BrandButton({ children, onClick, href }) {
+  if (href) {
+    return (
+      <a href={href} className="brand-button">
+        {children}
+      </a>
+    );
+  }
+  return (
+    <button className="brand-button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
