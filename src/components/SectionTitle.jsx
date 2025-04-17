51611a6c730b345ac3324d17c3ed30a795913977
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-17 23:19:50 +0900

    Update SectionTitle.jsx

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,12 +3,13 @@ import "./SectionTitle.css";
 
 export default function SectionTitle({ children, style = {} }) {
   const combinedStyle = {
-    marginTop: "24px",
-    marginBottom: "4px",
+    marginTop: "64px",
+    marginBottom: "4px", // 섹션과 붙이기 위한 최소 여백
     paddingLeft: style.textAlign === "left" ? "12px" : "0px",
     paddingRight: style.textAlign === "right" ? "12px" : "0px",
     fontSize: "20px",
     fontWeight: "600",
+    color: "#222",
     ...style,
   };
 
