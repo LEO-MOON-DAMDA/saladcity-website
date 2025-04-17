f22ab194fcd47397f50132120d28d3e36e4225d7
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-17 22:22:02 +0900

    Update InstaFeedSection.jsx

--- src/components/InstaFeedSection.jsx
+++ src/components/InstaFeedSection.jsx
@@ -5,17 +5,19 @@ const images = [
   "/images/insta01.jpg",
   "/images/insta02.jpg",
   "/images/insta03.jpg",
-  "/images/insta04.jpg"
+  "/images/insta04.jpg",
+  "/images/insta05.jpg",
+  "/images/insta06.jpg",
 ];
 
 export default function InstaFeedSection() {
   return (
-    <section className="insta-section">
-      <h2 className="insta-title">#Saladcity on Instagram</h2>
+    <section className="insta-feed-section">
+      <h2 className="insta-title">#saladcity on Instagram</h2>
       <div className="insta-grid">
         {images.map((src, index) => (
-          <div key={index} className="insta-image-wrapper">
-            <img src={src} alt={`insta${index + 1}`} className="insta-image" />
+          <div key={index} className="insta-item">
+            <img src={src} alt={`insta-${index}`} />
           </div>
         ))}
       </div>
