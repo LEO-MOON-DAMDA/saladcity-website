0c4e26fabfcf5d27cc33432897e9b89144491333
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-19 04:01:29 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Layout from "./components/Layout";
 import SaladcityHome from "./SaladcityHome";
 import MenuPage from "./components/MenuPage";
 import ReviewsPage from "./pages/Reviews";
-import MissionPage from "./pages/MissionPage"; // ✅ 추가
+import MissionPage from "./pages/MissionPage";
+import SocialPage from "./pages/SocialPage"; // ✅ 추가
 
 export default function App() {
   return (
@@ -13,9 +14,11 @@ export default function App() {
           <Route path="/" element={<SaladcityHome />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/mission" element={<MissionPage />} /> {/* ✅ 미션페이지 연결 */}
+          <Route path="/mission" element={<MissionPage />} />
+          <Route path="/social" element={<SocialPage />} /> {/* ✅ 추가 */}
         </Route>
       </Routes>
     </Router>
   );
 }
+
