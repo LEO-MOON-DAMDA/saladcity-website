f24e8a93173fd882f809a15cb5799f19bdc81d3d
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-23 21:18:17 +0900

    Create Success.jsx

---
+++ src/pages/Success.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Success() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h1 style={{ color: '#2c8f5b' }}>결제 성공!</h1>
+      <p>감사합니다. 결제가 완료되었습니다.</p>
+    </div>
+  );
+}
