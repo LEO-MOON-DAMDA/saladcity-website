c32c0c52a395f5dd05135a8aa24e417bae03f78d
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-23 21:18:35 +0900

    Create Fail.jsx

---
+++ src/pages/Fail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Fail() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h1 style={{ color: '#cc0000' }}>결제 실패</h1>
+      <p>문제가 발생했습니다. 다시 시도해 주세요.</p>
+    </div>
+  );
+}
