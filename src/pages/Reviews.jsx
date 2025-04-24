61abd98eff7b05fed087c714838988ab3975fe73
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-25 00:48:08 +0900

    Update Reviews.jsx

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -14,7 +14,7 @@ export default function Reviews() {
   const [selectedReview, setSelectedReview] = useState(null);
 
   useEffect(() => {
-    fetch("/data/reviews_baemin.json")
+    fetch("/data/review_with_emotion_random.json")
       .then((res) => res.json())
       .then((data) => {
         const clean = data.filter((r) => {
