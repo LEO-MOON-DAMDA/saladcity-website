7d7481bb7299fb61989b476f33bd09d782b0982e
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-30 14:10:23 +0900

    Update supabaseMenuClient.js

--- src/utils/supabaseMenuClient.js
+++ src/utils/supabaseMenuClient.js
@@ -1,8 +1,9 @@
 // src/utils/supabaseMenuClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bjcetaznlmqgjvozeeen.supabase.co"; // 메뉴용 URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqY2V0YXpubG1xZ2p2b3plZWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzI1MjksImV4cCI6MjA2MDEwODUyOX0.5Y86eiA_14SibBxOHjVU8p60lvPjj5BBT2WhQrd_5oE"; // 메뉴용 anon key
+// ✅ .env 파일에서 불러오도록 수정
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabaseMenu = createClient(
   supabaseUrl,
