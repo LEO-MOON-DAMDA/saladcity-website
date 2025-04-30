c42f990e34f7d1ff9afa5b484f79c0dba9376b8a
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-30 14:10:38 +0900

    Update supabaseOutpostClient.js

--- src/utils/supabaseOutpostClient.js
+++ src/utils/supabaseOutpostClient.js
@@ -1,8 +1,8 @@
 // src/utils/supabaseOutpostClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cgcfnxjscqglqxdbbypd.supabase.co"; // Outpost용 URL
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnY2ZueGpzY3FnbHF4ZGJieXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MTQ4NDYsImV4cCI6MjA2MTM5MDg0Nn0.ELrxGHeWhmhkcsDsXhERUcumby7j673QsbqB2WfcD4M"; // Outpost용 anon key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabaseOutpost = createClient(
   supabaseUrl,
